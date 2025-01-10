1b9c2d1af9758f172499c52f4e473d48f4105fce
Ammara Shakeel <ammarashakeel81@yahoo.se>
2025-01-10 10:28:27 +0100

    Entities

---
+++ Domain.Models/Entities/Activities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entities
+{
+    public class Activities
+    {
+        public int Id { get; set; } //primär nyckel
+        public int ModuleId { get; set; } //FK
+        public int ActivityTypeId { get; set; } //FK
+
+        public Module? Module { get; set; } //NP
+        public ActivityType? ActivityType { get; set; } //NP
+        public string ActivityName { get; set; } = string.Empty;
+        public string ActivityDescription { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ Domain.Models/Entities/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entities
+{
+    public class Module
+    {
+        public int Id { get; set; } //primär nyckel
+        public int CourseId { get; set; } //foreign key
+        public Course Course { get; set; } = new Course(); //navigational property
+        public string ModuleName { get; set; } = string.Empty;
+        public string ModuleDescription { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
