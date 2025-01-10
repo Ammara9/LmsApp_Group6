73695c2b4b02b8a67ce6ed15c7587ae41a335682
Ammara Shakeel <ammarashakeel81@yahoo.se>
2025-01-10 09:07:16 +0100

    Add project files.

---
+++ Domain.Models/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Models.Entities;
+
+//Separate ApplicationUser between projects
+//Setup relationship with EF here!
+public class ApplicationUser : IdentityUser
+{
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpireTime { get; set; }
+}
---
+++ LMS.Infrastructure/Data/AutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Domain.Models.Entities;
+using LMS.Shared.DTOs;
+namespace LMS.Infrastructure.Data;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<UserForRegistrationDto, ApplicationUser>();
+    }
+}
